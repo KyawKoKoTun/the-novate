52cc47165573d2b2f0d9a42ffc19c1eca5e01bb0
Kyaw Ko Ko Tun <kyawkokotunmm475157@gmail.com>
2024-02-26 01:56:56 +0630

    nav

--- src/components/Well.jsx
+++ src/components/Well.jsx
@@ -1,7 +1,7 @@
 export default function Well({ children }) {
   return (
     <>
-      <div className="px-[20px] my-12 sm:px-[80px] md:px-[140px] lg:px-[200px] py-16 bg-zinc-950">
+      <div className="px-[20px] my-12 sm:px-[80px] md:px-[140px] lg:px-[200px] py-20 bg-zinc-950">
         {children}
       </div>
     </>
